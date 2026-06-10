fcfce5400c1ae190641e1c7be8ca6f0e0e20b055
Sunil <om.gamal10@gmail.com>
2026-06-10 16:15:31 +0545

    fix bugs

--- src/services/async.error.handling.ts
+++ src/services/async.error.handling.ts
@@ -3,11 +3,12 @@ import { NextFunction, Request, Response } from "express"
 const asyncErrorHandler = (fn: Function) => {
   return (req: Request, res: Response, next: NextFunction) => {
     fn(req, res, next).catch((err: Error) => {
+      console.error(err);
       return res.status(500).json({
-        message : err.message,
-        fullError : err
-      })
-    })
+        message: err.message,
+        fullError: err,
+      });
+    });
   }
 }
 
